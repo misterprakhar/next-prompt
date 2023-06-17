157ae6486d98545ac7194e37989e79a39aea8bfa
Prakhar Srivastav <prak13580@gmail.com>
2023-06-17 12:48:04 +0530

    Modify headers to feed route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async (request) => {
     try {
         await connectToDB()
         const prompts = await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prompts), { status: 200, headers: { 'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate' } })
+        return new Response(JSON.stringify(prompts), { status: 200, headers: { 'Cache-Control': 'no-cache' } })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
