e5ce371a2b1ebc85681c8f1ed1b29af461cefc4a
Prakhar Srivastav <prak13580@gmail.com>
2023-06-17 13:32:28 +0530

    Changed route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,15 +1,22 @@
-import Prompt from "@models/prompt"
-import { connectToDB } from "@utils/database"
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
+import { headers } from 'next/headers';
 
 export const GET = async (request) => {
+    const headersList = headers();
+    const referer = headersList.get('referer');
     try {
-        await connectToDB()
-        const prompts = await Prompt.find({}).populate('creator')
-        const response = new Response(JSON.stringify(prompts), { status: 200 })
-        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        await connectToDB();
 
-        return response
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                referer: referer,
+                'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+            },
+        });
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        return new Response('Failed to fetch all prompts', { status: 500 });
     }
-}
+};
