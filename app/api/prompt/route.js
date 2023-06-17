ac45ddc0bbd212238e05fea8ebd715c46a2cff48
Prakhar Srivastav <prak13580@gmail.com>
2023-06-17 13:19:58 +0530

    Modify previously set headers

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,9 +7,7 @@ export const GET = async (request) => {
         const prompts = await Prompt.find({}).populate('creator')
         const response = new Response(JSON.stringify(prompts), { status: 200 })
         response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
-        response.headers.set("Pragma", "no-cache");
-        response.headers.set("Expires", "0");
-        response.headers.set("Location", url.toString());
+
         return response
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
